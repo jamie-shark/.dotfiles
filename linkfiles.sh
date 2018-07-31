c0350135c0e06e74c7d4acebe3a65a83d2fe6ab9
Jon Campbell <jonathan.campbell93@ntlworld.com>
2018-07-31 21:04:33 +0100

    Add setup script for vundle and include linkfiles

--- linkfiles.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-ln ./home/.vimrc ~/.vimrc -f
-ln ./home/.bashrc ~/.bashrc -f
-ln ./home/.inputrc ~/.inputrc -f
-echo "dotfiles linked back to homedir"
-
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+ln ./home/.vimrc ~/.vimrc -f
+ln ./home/.bashrc ~/.bashrc -f
+ln ./home/.inputrc ~/.inputrc -f
+mkdir -p ~/.vim/bundle
+git clone -q https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
