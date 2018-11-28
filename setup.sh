959dda04eeafd8f5972d0e34ff4d95f66b8a6895
Jon Campbell <jonathan.campbell@firsthelpline.com>
2018-11-28 11:56:44 +0000

    Neaten up setup.sh output

--- setup.sh
+++ setup.sh
@@ -5,15 +5,18 @@ ln ./home/.bashrc ~/.bashrc -f
 ln ./home/.inputrc ~/.inputrc -f
 ln ./home/.minttyrc ~/.minttyrc -f
 
+echo "Sourcing bashrc . . ."
+source ~/.bashrc
+
 echo "Installing Vundle . . ."
 mkdir -p ~/.vim/bundle
-git clone -q https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone -q https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim > /dev/null 2>&1
 
-echo "Installing Post Git . . ."
-git clone -q https://github.com/lyze/posh-git-sh.git ~/.dotfiles/posh-git-sh
+echo "Installing Posh Git . . ."
+git clone -q https://github.com/lyze/posh-git-sh.git ~/.dotfiles/posh-git-sh > /dev/null 2>&1
 
 echo "Installing diffconflicts . . ."
-curl --url https://raw.githubusercontent.com/whiteinge/dotfiles/master/bin/diffconflicts --output ~/bin/diffconflicts --create-dirs
+curl https://raw.githubusercontent.com/whiteinge/dotfiles/master/bin/diffconflicts -o ~/bin/diffconflicts > /dev/null 2>&1 && chmod +x ~/bin/diffconflicts
 
 echo "Installing diff-highlight . . ."
 curl https://raw.githubusercontent.com/git/git/fd99e2bda0ca6a361ef03c04d6d7fdc7a9c40b78/contrib/diff-highlight/diff-highlight -o ~/bin/diff-highlight > /dev/null 2>&1 && chmod +x ~/bin/diff-highlight
@@ -21,6 +24,6 @@ curl https://raw.githubusercontent.com/git/git/fd99e2bda0ca6a361ef03c04d6d7fdc7a
 platform=$(uname -s)
 if [[ $platform = CYGWIN* ]] || [[ $platform = MINGW* ]] ; then
     echo "Linking Powershell profile . . ."
-    mkdir ~/Documents/WindowsPowerShell
+    mkdir -p ~/Documents/WindowsPowerShell
     ln ./home/Microsoft.PowerShell_profile.ps1 ~/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1 -f
 fi
