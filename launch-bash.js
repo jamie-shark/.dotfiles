d1206445599f23675ea8ab0719d945e284cc65eb
Jon Campbell <jonathan.campbell@firsthelpline.com>
2019-02-27 16:28:46 +0000

    Replace launch-bash.js with terminator lnk

--- launch-bash.js
+++
@@ -1,2 +0,0 @@
-WScript.CreateObject("WScript.Shell").run('bash.exe -l -c "DISPLAY=:0.0 terminator"', 0, false);
-
---
+++ terminator.ps1
@@ -0,0 +1,2 @@
+&"C:\Program Files\VcXsrv\vcxsrv.exe" :0 -ac -terminate -lesspointer -multiwindow -clipboard -wgl -dpi auto
+bash.exe -l -c "DISPLAY=:0 terminator --no-dbus"
