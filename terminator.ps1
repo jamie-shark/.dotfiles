645fd91fce13ad52146e32884f85d3f0ae11f2e6
jon@DEVWRKSTN00428 <jon@DEVWRKSTN00428>
2019-02-27 17:02:09 +0000

    Have terminator.ps1 only launch X window server if not already running

--- terminator.ps1
+++ terminator.ps1
@@ -1,2 +1,6 @@
-&"C:\Program Files\VcXsrv\vcxsrv.exe" :0 -ac -terminate -lesspointer -multiwindow -clipboard -wgl -dpi auto
+if (-not (Get-Process -Name vcxsrv -ErrorAction Ignore))
+{
+    &"C:\Program Files\VcXsrv\vcxsrv.exe" :0 -ac -terminate -lesspointer -multiwindow -clipboard -wgl -dpi auto
+}
+
 bash.exe -l -c "DISPLAY=:0 terminator --no-dbus"
