c3bb87b2ee403c94abc4895d431693ec599ab3fd
jon@DEVWRKSTN00428 <jon@DEVWRKSTN00428>
2019-02-27 18:35:14 +0000

    Remove unneeded lines from wsl-setup

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -1,5 +1,2 @@
 sudo apt update && sudo apt upgrade -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
 sudo apt update && sudo apt install terminator
-export DISPLAY=:0.0
-sudo sed -i 's$<listen>.*</listen>$<listen>tcp:host=localhost,port=0</listen>$' /etc/dbus-1/session.conf
-terminator
