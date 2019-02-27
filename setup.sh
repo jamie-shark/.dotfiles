e347aa22b45da5ee6df9a63a18ee89b83e5e7169
Jon Campbell <jonathan.campbell@firsthelpline.com>
2019-02-27 14:18:13 +0000

    Remove getops and make script more verbose

--- setup.sh
+++ setup.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env sh
 
-pluginInstall=false
-
-while getopts ":p" opt; do
-    case $opt in
-        p) pluginInstall=true ;;
-        \?) ;;
-    esac
-done
-
-echo "Setting git configs . . ."
+echo "### Setting git configs ###"
 git config --global color.ui auto
 git config --global core.autocrlf true
 git config --global core.safecrlf false
@@ -30,41 +21,39 @@ git config --global pager.show 'diff-highlight | less'
 git config --global pager.diff 'diff-highlight | less'
 git config --global interactive.diffFilter diff-highlight
 
-echo "Linking dotfiles . . ."
+echo "### Linking dotfiles ###"
 ln ./home/.vimrc ~/.vimrc -f
 ln ./home/.bashrc ~/.bashrc -f
 ln ./home/.inputrc ~/.inputrc -f
 ln ./home/.minttyrc ~/.minttyrc -f
 
-echo "Sourcing bashrc . . ."
+echo "### Sourcing bashrc ###"
 source ~/.bashrc
 
-echo "Installing VimPlug . . ."
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null 2>&1 && chmod +x ~/.local/share/nvim/site/autoload/plug.vim
+echo "### Installing VimPlug ###"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "Installing Posh Git . . ."
-git clone -q https://github.com/lyze/posh-git-sh.git ~/.dotfiles/posh-git-sh > /dev/null 2>&1
+echo "### Installing Posh Git ###"
+git clone https://github.com/lyze/posh-git-sh.git ~/.dotfiles/posh-git-sh
 
-echo "Installing diffconflicts . . ."
-curl https://raw.githubusercontent.com/whiteinge/dotfiles/master/bin/diffconflicts -o ~/bin/diffconflicts > /dev/null 2>&1 && chmod +x ~/bin/diffconflicts
+echo "### Installing diffconflicts ###"
+curl https://raw.githubusercontent.com/whiteinge/dotfiles/master/bin/diffconflicts -o ~/bin/diffconflicts
 
-echo "Installing diff-highlight . . ."
-curl https://raw.githubusercontent.com/git/git/fd99e2bda0ca6a361ef03c04d6d7fdc7a9c40b78/contrib/diff-highlight/diff-highlight -o ~/bin/diff-highlight > /dev/null 2>&1 && chmod +x ~/bin/diff-highlight
+echo "### Installing diff-highlight ###"
+curl https://raw.githubusercontent.com/git/git/fd99e2bda0ca6a361ef03c04d6d7fdc7a9c40b78/contrib/diff-highlight/diff-highlight -o ~/bin/diff-highlight
 
-echo "Installing fonts . . ."
+echo "### Installing fonts ###"
 mkdir -p ~/.fonts
 cp GoMono-NF.ttf ~/.fonts/
 
 platform=$(uname -s)
 if [[ $platform = CYGWIN* ]] || [[ $platform = MINGW* ]] ; then
     ln ./home/.vimrc ~/_vimrc -f
-    echo "Linking Powershell profile . . ."
+    echo "### Linking Powershell profile ###"
     mkdir -p ~/Documents/WindowsPowerShell
     ln ./home/Microsoft.PowerShell_profile.ps1 ~/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1 -f
     ln ./home/ConEmu.xml ~/AppData/Roaming/ConEmu.xml -f
 fi
 
-if $pluginInstall ; then
-    echo "Installing Vim plugins . . ."
-    vim -c 'call InstallPlugins()' -c 'qa!'
-fi
+echo "### Installing Vim plugins ###"
+vim -c 'call InstallPlugins()' -c 'qa!'
