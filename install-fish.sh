c51ec8f2db09094702fcd6e2871034d96a3c9817
jon@DEVWRKSTN00428 <jon@DEVWRKSTN00428>
2019-02-27 19:29:28 +0000

    Add alternate shell install scripts

---
+++ install-fish.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+sudo apt update && sudo apt install curl fish
+curl -L http://get.oh-my.fish | fish
---
+++ install-zsh.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+sudo apt update && sudo apt install curl xsh
+curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
