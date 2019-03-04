040b560a539a2a0580034a9207913ce7f202812a
jon@DEVWRKSTN00428 <jon@DEVWRKSTN00428>
2019-03-04 18:29:09 +0000

    Add Documents symlink to WSL setup

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -1,2 +1,4 @@
 sudo apt update && sudo apt upgrade -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
-sudo apt update && sudo apt install terminator
+sudo apt update && sudo apt install terminator -y
+ln -fs /mnt/c/Users/jonathan.campbell/Downloads/ ~/
+ln -fs /mnt/c/Users/jonathan.campbell/Documents/ ~/
