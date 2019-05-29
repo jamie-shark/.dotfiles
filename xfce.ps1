217df7f407868c917cb81d752d1ecbcc93e865e6
jon <jonathan.campbell@comparisontech.com>
2019-05-29 21:30:15 +0100

    Add xfce-session shortcut

---
+++ xfce.ps1
@@ -0,0 +1,11 @@
+if (-not (Get-Process -Name vcxsrv -ErrorAction Ignore))
+{
+    &"C:\Program Files\VcXsrv\vcxsrv.exe" :0 -ac -terminate -lesspointer -multiwindow -clipboard -wgl -dpi auto
+}
+
+if (-not (Get-Process -Name pulseaudio -ErrorAction Ignore))
+{
+    &"C:\pulseaudio-1.1\bin\pulseaudio.exe" --use-pid-file=false -D
+}
+
+bash.exe -l -c "DISPLAY=:0 xfce4-session"
