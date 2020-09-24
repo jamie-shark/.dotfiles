b971c18eb7df3d462108a387d72faf993d4b6c2a
jon@DEVWRKSTN00428 <jonathan.campbell@comparisontech.com>
2020-09-24 11:22:09 +0100

    Add auto-suggestions to zsh install script

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+cd ~
 sudo apt update && sudo apt install curl zsh
 curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 chsh -s $(which zsh)
+cd -
