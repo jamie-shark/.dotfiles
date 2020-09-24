c6807f05fd01dd66f0a29bfeb5f281780a9d15d4
jon@DEVWRKSTN00428 <jonathan.campbell@comparisontech.com>
2020-09-24 11:10:22 +0100

    Update termintor bootstrap script for wsl 2

--- terminator.ps1
+++ terminator.ps1
@@ -8,4 +8,13 @@ if (-not (Get-Process -Name pulseaudio -ErrorAction Ignore))
     &"C:\pulseaudio-1.1\bin\pulseaudio.exe" --use-pid-file=false -D
 }
 
-bash.exe -l -c "DISPLAY=:0 terminator"
+if (Get-Command "wsl" -ErrorAction SilentlyContinue)
+{
+    Write-Output "exec wsl"
+    wsl -d "Ubuntu-20.04" -e "DISPLAY=:0.0 terminator"
+}
+else
+{
+    Write-Output "exec bash"
+    bash.exe -l -c "DISPLAY=:0.0 terminator"
+}
