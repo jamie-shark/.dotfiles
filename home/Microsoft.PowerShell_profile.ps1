297fb6cd9369a51846a432a8356fde23bb921c2b
Jamie Campbell <jonathan.campbell93@ntlworld.com>
2021-01-13 19:34:37 +0000

    Start pulseaudio on powershell start
    Signed-off-by: jamie <jonathan.campbell93@ntlworld.com>

--- home/Microsoft.PowerShell_profile.ps1
+++ home/Microsoft.PowerShell_profile.ps1
@@ -161,3 +161,7 @@ if (-not (Get-Process -Name vcxsrv -ErrorAction Ignore))
     &"C:\Program Files\VcXsrv\vcxsrv.exe" :0 -ac -terminate -lesspointer -multiwindow -clipboard -wgl -dpi auto
 }
 
+if (-not (Get-Process -Name pulseaudio -ErrorAction Ignore))
+{
+    &"C:\pulseaudio-1.1\bin\pulseaudio.exe" --use-pid-file=false -D
+}
